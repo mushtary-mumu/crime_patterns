7bca91dfe0b552031a22306622119f788c20c2c2
mushtary-mumu <mushtarymumu@gmail.com>
2023-03-29 09:43:38 +0200

    updates analysis tests

--- tests/analysis/test_point_patterns.py
+++ tests/analysis/test_point_patterns.py
@@ -9,57 +9,25 @@ from shapely.geometry import box
 
 DESIRED_PRECISION = 10e-2
 
-# @pytest.fixture()
-# def data():
-#     np.random.seed(0)
-#     x = np.random.normal(size=100_000)
-#     coef = 2.0
-#     prob = 1 / (1 + np.exp(-coef * x))
-#     return pd.DataFrame(
-#         {"outcome_numerical": np.random.binomial(1, prob), "covariate": x},
-#     )
-
-# @pytest.fixture()
-# def data_info():
-#     return {"outcome": "outcome", "outcome_numerical": "outcome_numerical"}
-
-
-# def test_fit_logit_model_recover_coefficients(data, data_info):
-#     model = fit_logit_model(data, data_info, model_type="linear")
-#     params = model.params
-#     assert np.abs(params["Intercept"]) < DESIRED_PRECISION
-#     assert np.abs(params["covariate"] - 2.0) < DESIRED_PRECISION
-
-
-# def test_fit_logit_model_error_model_type(data, data_info):
-#     with pytest.raises(ValueError):  # noqa: PT011
-#         assert fit_logit_model(data, data_info, model_type="quadratic")
 #%%
-@pytest.fixture()
-def data():
-
-    longitudes = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    latitudes = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-    region = gpd.GeoDataFrame(geometry=[box(0, 0, 1, 1)], crs="EPSG:4326")
-
-    return {"longitudes":longitudes, "latitudes":latitudes, "region":region}
+def test_evaluate_hotspots(mock_crime_points, mock_crime_polygons):
+    
+    ds = evaluate_hotspots( longitudes = mock_crime_points["points"].x, 
+                            latitudes = mock_crime_points["points"].y, 
+                            region = mock_crime_polygons, 
+                            crs="EPSG:4326")
+    
+    assert np.isclose(float(ds.densities.max()), 11.90496, atol=pytest.DESIRED_PRECISION)
+    assert np.isclose(float(ds.densities.min()), 1.00051, atol=pytest.DESIRED_PRECISION)
+    assert np.isclose(float(ds.densities.mean()), 4.53039, atol=pytest.DESIRED_PRECISION)
 
 #%%
-@pytest.mark.skip(reason="not finalized yet")
-def test_evaluate_hotspots(data):
+def test_cluster_crime_incidents_dbscan(mock_crime_points):
     
-    ds = evaluate_hotspots(data["longitudes"], data["latitudes"], data["region"])
+    cluster_labels = cluster_crime_incidents_dbscan(longitudes = mock_crime_points["points"].x, 
+                                                    latitudes = mock_crime_points["points"].y, 
+                                                    epsilon=6, 
+                                                    min_samples=10)
     
-    assert ds.densities.max() == 1.0
-    assert ds.densities.min() == 1.0
-
-#%%
-@pytest.mark.skip(reason="not finalized yet")
-def test_cluster_crime_incidents_dbscan(data):
-    
-    cluster_labels = cluster_crime_incidents_dbscan(data["longitudes"], data["latitudes"],epsilon=0.1,min_samples=1)
-    
-    assert cluster_labels.max() == 0
-    assert cluster_labels.min() == 0
+    assert set(cluster_labels.labels_) == {-1,  0,  1,  2}
 
